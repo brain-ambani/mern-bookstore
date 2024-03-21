8e370dc4620e2e97282cc0e4cf32fbed9f97530e
brian-ambani <brainambani1@gmail.com>
2024-03-22 01:36:04 +0300

    add cors package

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 dotenv.config();
 const port = process.env.PORT || 3300;
@@ -11,6 +12,18 @@ const app = express();
 
 // Middleware for JSON body parsing
 app.use(express.json());
+
+// Middleware for CORS
+// Option 1: Allow all origins with Default CORS(*)
+// app.use(cors());
+// Option 2: Allow Custom Origin
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type",
+  })
+);
 
 app.use("/api/books", booksRoute);
 
