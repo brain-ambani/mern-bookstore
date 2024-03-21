ad3dfeeea87a8c9f459879baf7165d728e1aa2c9
brian-ambani <brainambani1@gmail.com>
2024-03-22 02:36:14 +0300

    creat show book ui

--- client/src/pages/ShowBook.jsx
+++ client/src/pages/ShowBook.jsx
@@ -1,4 +1,58 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Spinner from "../components/Spinner";
+import { useParams } from "react-router-dom";
+import BackButton from "../components/BackButton";
+
 const ShowBook = () => {
-  return <div>ShowBook</div>;
+  const [book, setBook] = useState({});
+  const [loading, setLoading] = useState(false);
+  const { id } = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`http://localhost:3300/api/books/${id}`)
+      .then((response) => {
+        setBook(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <div className="ml-4 p-4">
+      <BackButton />
+      <h1 className="text-3xl my-4">Show Book</h1>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className="flex flex-col border-sky-800 rounded-xl w-fit p-4">
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500">Title:</span>
+            <span>{book.title}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500">Author:</span>
+            <span>{book.author}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500">Publish Year:</span>
+            <span>{book.publishYear}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500">Created Time:</span>
+            <span>{new Date(book.createdAt).toString()}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500">Updated Time:</span>
+            <span>{new Date(book.updatedAt).toString()}</span>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 export default ShowBook;
