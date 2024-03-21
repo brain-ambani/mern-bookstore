77e26e16a93043adbc21119e3c93673af5b3f505
brian-ambani <brainambani1@gmail.com>
2024-03-21 18:43:37 +0300

    updated the bookController function for the API end points

--- server/controllers/booksController.js
+++ server/controllers/booksController.js
@@ -18,4 +18,66 @@ const newBook = async (req, res) => {
   }
 };
 
-export default newBook;
+// get all books
+
+const getBooks = async (req, res) => {
+  const books = await Book.find().sort({ createdAt: -1 });
+
+  return res.status(200).send({
+    count: books.length,
+    data: books,
+  });
+};
+
+// get a single book by ID
+
+const getBookById = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("Book not found");
+
+  const book = await Book.findById(id);
+
+  if (!book) return res.status(404).send("Book not found");
+
+  return res.status(200).send(book);
+};
+
+// update a book by ID
+
+const updateBook = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("Book not found");
+
+  const book = await Book.findOneAndUpdate({ _id: id }, { ...req.body });
+
+  res.status(200).send(book);
+};
+
+// delete a book by ID
+
+const deleteBook = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("Book not found");
+
+  const book = await Book.findOneAndDelete({ _id: id });
+
+  if (!book) return res.status(404).send("Book not found");
+
+  res.status(200).send(book);
+};
+
+// export the controllers
+
+export const booksController = {
+  newBook,
+  getBooks,
+  getBookById,
+  updateBook,
+  deleteBook,
+};
--- server/routes/booksRoute.js
+++ server/routes/booksRoute.js
@@ -1,68 +1,25 @@
 import express from "express";
 import { Book } from "../models/bookModel.js";
-import newBook from "../controllers/booksController.js";
+
+import { booksController } from "../controllers/booksController.js";
 
 const router = express.Router();
 
 // Route to save a new book
-router.post("/", newBook);
+router.post("/", booksController.newBook);
 
 // Route to get all books
-router.get("/", async (req, res) => {
-  try {
-    const books = await Book.find().sort({ createdAt: -1 });
-
-    return res.status(200).send({
-      count: books.length,
-      data: books,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({ message: error.message });
-  }
-});
+router.get("/", booksController.getBooks);
 
 // Route to get a single book by ID
-router.get("/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const book = await Book.findById({ _id: id });
-
-    res.status(200).send(book);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({ message: error.message });
-  }
-});
+router.get("/:id", booksController.getBookById);
 
 // Route for updating a book by ID
 
-router.patch("/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    const book = await Book.findByIdAndUpdate({ _id: id }, { ...req.body });
-
-    res.status(200).send(book);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({ message: error.message });
-  }
-});
+router.patch("/:id", booksController.updateBook);
 
 // Route for deleting a book by ID
 
-router.delete("/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    const book = await Book.findByIdAndDelete({ _id: id });
-
-    res.status(200).send(book);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({ message: error.message });
-  }
-});
+router.delete("/:id", booksController.deleteBook);
 
 export default router;
