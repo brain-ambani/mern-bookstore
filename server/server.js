c88abf0acd1a208ae55b128f3a0e773292d0423f
brian-ambani <brainambani1@gmail.com>
2024-03-21 17:29:28 +0300

    add the delete route

--- server/server.js
+++ server/server.js
@@ -62,6 +62,37 @@ app.get("/books/:id", async (req, res) => {
     res.status(500).send({ message: error.message });
   }
 });
+
+// Route for updating a book by ID
+
+app.patch("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const book = await Book.findByIdAndUpdate({ _id: id }, { ...req.body });
+
+    res.status(200).send(book);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+// Route for deleting a book by ID
+
+app.delete("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const book = await Book.findByIdAndDelete({ _id: id });
+
+    res.status(200).send(book);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: error.message });
+  }
+});
+
 mongoose
   .connect(MongoDB)
   .then(() => {
