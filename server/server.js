cc1a8154070c7412314ab007665f1fadeeb77aaa
brian-ambani <brainambani1@gmail.com>
2024-03-21 16:52:42 +0300

    add POST route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 dotenv.config();
 const port = process.env.PORT || 3300;
@@ -8,8 +9,30 @@ const MongoDB = process.env.MONGODB_URI;
 
 const app = express();
 
+// Middleware for JSON body parsing
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.status(200).send("Hello World");
+});
+
+// Route to save a new book
+app.post("/books", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear)
+      return res.status(400).send("All fields are required");
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear,
+    };
+    const book = await Book.create(newBook);
+
+    res.status(201).send(book);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: error.message });
+  }
 });
 
 mongoose
